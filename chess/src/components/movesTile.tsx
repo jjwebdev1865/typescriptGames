2b8d66049ea7ffd6c5e52401e3a24db707b1358c
Jim Jiracek <jim.jiracek2@gmail.com>
2024-08-30 22:44:50 -0400

    two step move process

--- chess/src/components/movesTile.tsx
+++ chess/src/components/movesTile.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { PlayersContext } from "../context/players.context"
 import { PieceInfo, Player } from "../types/models"
 import { GameContext } from "../context/game.context"
@@ -8,28 +8,53 @@ interface MovesTileProps {
 }
 export const MovesTile = ({ player }: MovesTileProps) => {
   const { changeTurn } = useContext(GameContext)
-  const {  handlePieceMove, getMoveOptions } = useContext(PlayersContext)
+  const { handlePieceMove, getMoveOptions } = useContext(PlayersContext)
+  const [moveOptions, setMoveOptions] = useState([] as (string | PieceInfo)[][])
+
+  const handleMoveOptions = (piece: string, pieceInfo: PieceInfo) => {
+    const newPosition = getMoveOptions(pieceInfo.position)
+
+    const newMoveOption = [piece, {
+      ...pieceInfo,
+      position: newPosition
+    }]
+    
+    setMoveOptions([newMoveOption])
+  }
 
   const handlePawnMove = (piece: string, pieceInfo: PieceInfo) => {
-    // TODO: get move options
-    const newPosition = getMoveOptions(pieceInfo.position)
-    handlePieceMove(piece, {
-      name: pieceInfo.name,
-      position: newPosition
-    })
+    handlePieceMove(piece, pieceInfo)
+    setMoveOptions([])
     // changeTurn()
   }
+
+  const displayMoves = () => {
+    return (
+      <ul style={{ listStyle: 'none', display: 'flex'}}>
+        {moveOptions.map(option => {
+          const piece = option[0] as string
+          const pieceInfo = option[1] as PieceInfo
+          return (
+            <li key={`move-option-${option}`}>
+              <button onClick={() => handlePawnMove(piece, pieceInfo)}>{pieceInfo.position}</button> 
+            </li>
+          )}
+        )}
+      </ul>
+    )
+  }
+
 
   return (
     <div>
       <ul style={{ listStyle: 'none', display: 'flex'}}>
         {Object.entries(player.pieces).map(([key, value]) => (
           <li key={key}>
-            <button onClick={() => handlePawnMove(key, value)}>{key}</button>
+            <button onClick={() => handleMoveOptions(key, value)}>{key}</button>
           </li>
         ))}
-
       </ul>
+      {moveOptions.length > 0 && displayMoves()}
     </div>
   )
 }
