7eee529961d62a8ab56940073433f04a04bc972a
Jim Jiracek <jim.jiracek2@gmail.com>
2024-09-10 09:20:49 -0400

    rook block poisition

--- chess/src/utils/getMoveList.ts
+++ chess/src/utils/getMoveList.ts
@@ -9,16 +9,25 @@ export function GetMoveList
   isPlayerPieceInPosition: (newPosition: string) => boolean
 ): any[] {
   const newMoveList: any[] = []
+  let blockedLetter = ''
   movePositions.forEach(move => {
     const isOpponentPieceInPosition = isOpponentInPosition(move)
     const isMyPieceInPosition = isPlayerPieceInPosition(move)
+    let isPieceBlockingTheWay = false
+    if (isMyPieceInPosition && pieceInfo.name.toLocaleLowerCase() === 'rook') {
+      blockedLetter = move.split('')[0]
+    }
+    if (pieceInfo.name.toLocaleLowerCase() === 'rook' && move.split('')[0] === blockedLetter) {
+      isPieceBlockingTheWay = true
+    }
+
     const newMove = [
       piece,
       {
         ...pieceInfo,
         position: move
       },
-      (isMyPieceInPosition || isOpponentPieceInPosition) ? true : false
+      (isMyPieceInPosition || isOpponentPieceInPosition || isPieceBlockingTheWay) ? true : false
     ]
     newMoveList.push(newMove)
   })
