c82d9002afb403e2bd1158e71c1aed9f81f14200
jjwebdev1865 <52927050+jjwebdev1865@users.noreply.github.com>
2025-03-04 14:36:16 -0500

    Ttt init (#7)
    * removing temp code
    
    * tic tac toe init

---
+++ tic_tac_toe/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <h2>Goal is to incorporate AI to play against</h2>
+      <h3>This is a best of 3 game</h3>
+
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
