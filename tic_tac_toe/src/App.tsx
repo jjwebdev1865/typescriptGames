d4cd2d616f6f83677a69c09c758ca9ab4e7128cd
Jim Jiracek <jim.jiracek2@gmail.com>
2025-03-05 09:00:24 -0500

    board init

--- tic_tac_toe/src/App.tsx
+++ tic_tac_toe/src/App.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
 function App() {
+  const rows = ['A', 'B', 'C']
+
+  function buildBoardRows() {
+    return (
+      <div>
+        {rows.map(row => {
+          return (
+            <ul style={{ listStyle: 'none', display: 'flex', margin: 0}}>
+              <li style={{ padding: '25px', border: '1px solid black'}}>
+                spot
+              </li>
+              <li style={{ padding: '25px', border: '1px solid black'}}>
+                spot
+              </li>
+              <li style={{ padding: '25px', border: '1px solid black'}}>
+                spot
+              </li>
+            </ul>
+          )
+        })}
+      </div>
+    )
+  }
 
   return (
     <div className="App" style={{ textAlign: 'center'}}>
@@ -8,41 +31,7 @@ function App() {
       <h2>Goal is to incorporate AI to play against</h2>
       <h3>This is a best of 3 game</h3>
 
-      <div >
-        <ul style={{ listStyle: 'none', display: 'flex', margin: 0}}>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-        </ul>
-        <ul style={{ listStyle: 'none', display: 'flex', margin: 0}}>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-        </ul>
-        <ul style={{ listStyle: 'none', display: 'flex', margin: 0}}>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-          <li style={{ padding: '25px', border: '1px solid black'}}>
-            spot
-          </li>
-        </ul>
-      </div>
+      {buildBoardRows()}
     </div>
   );
 }
